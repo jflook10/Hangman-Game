749f8af4e1a9d2e7cbcf144e90f8f91f671f1c2c
Jillian Flook <jkenw10@gmail.com>
2017-03-16 17:08:06 -0500

    changes to js and html

---
+++ hangman.js
@@ -0,0 +1,75 @@
+
+var wordLib = ["quinoa", "bespoke", "sushi", "sustainable", "totally", "rush", "frappuccino"];
+
+//pull a word from the wordbank
+var wordLibLength = wordLib.length; //library could be an array of words. 
+var playerWord = "";
+var letterArray;
+
+	//Whole function selects a word and sets the underlines
+    function selectWord(){
+		//select word from array wordLib
+		playerWord = wordLib[Math.floor(Math.random()* 7)];
+		
+		var wordLength = letterArray.length;
+		//console.log(wordLength);
+
+		//turn the playerWord into an array
+		letterArray = playerWord.split("");
+		console.log(letterArray);
+
+		var wordPlaceHolder = document.getElementById("wordSpace");
+
+		//loop through length of the selected word to place a '_' where a letter would go
+		for (var i = 0; i < wordLength; i++){
+			//make a variable that creates a div. this placeholder will get overwritten with each iteration of the loop. 
+	      var newSpan = document.createElement("span");
+	      
+	      //sets the value of the innerHTML of the div to the array at index
+	      newSpan.innerHTML = "_ ";
+
+	      //adds the value of index to the page without losing the previous responses. 
+	      var addHashtag = wordPlaceHolder.appendChild(newSpan);
+	      //console.log(addHashtag);
+	}
+};
+
+
+var userGuessArr = []; //ASK WHY THIS HAS TO BE GLOBAL TO CONSOLE CORRECTLY
+document.onkeyup = function(event) {
+	
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(userGuess);
+	
+	userGuessArr.push(userGuess);
+	console.log(userGuessArr);
+
+	showInstruction();
+
+	//each userGuess, loop through
+	for(var i = 0; i < letterArray.length; i++ ){
+		console.log(letterArray[i]);
+	} 
+
+};
+
+//show instruction, "press any alpha character, space, or dash to start your guess". after first hit, hide instruction
+function showInstruction() {
+	document.getElementById("instructions").innerHTML= "Press any alpha-key, 'space', or 'dash' to make a guess";
+}
+
+
+//hide word but leave underline 
+
+//user guesses letter and triggers onkeyup
+
+// if guess is correct, 
+	// show letter from the hidden word list
+	// if letters remaining counter = 0 , or no hidden letters, alert you win
+
+// if guess is incorrect;
+	// add letter to the letters guessed array - push, pop
+	// decrease lives counter by 1
+	// if lives remaining = 0, alert you lose
+
+// 
