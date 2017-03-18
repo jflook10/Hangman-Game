6c32b67fa398f0d9052cc47945805b329ac4f501
Jillian Flook <jkenw10@gmail.com>
2017-03-18 12:00:10 -0500

    updated reset button

--- hangman.js
+++ hangman.js
@@ -1,6 +1,4 @@
- function loadFunction() {
-
-  
+ 
   var categories;         // Array of topics
   var chosenCategory;     // Selected catagory
   var word ;              // Selected word
@@ -10,6 +8,7 @@
   var counter ;           // Count correct guesses
   var space;              // Number of spaces in word '-'
   var userGuess = [];          // hols user guess to display to page
+  var acceptInput = true  // accepts user input. set to false when game is done
 
   var accepted = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' '];
 
@@ -17,13 +16,50 @@
   var showLives = document.getElementById("mylives");
   var userGuessSection = document.getElementById("userGuess");
   var gameImage = document.getElementsByClassName("gameImage");
+  var wordHolder = document.getElementById('hold');
 
+function loadFunction() {
+  
+  //onkeyup function
+  function userEvent(){
+    document.onkeyup = function(event) {
+      var guess = String.fromCharCode(event.keyCode).toLowerCase();
+      if(accepted.includes(guess)){
+        if(userGuess.indexOf(guess) == -1){
+          if(acceptInput){
+            for (var i = 0; i < word.length; i++) {
+              //replace word guess space with correct letter
+              if (word[i] === guess) {
+                  guesses[i].innerHTML = guess;
+                  counter += 1;
+                  }
+            }
+              var j = (word.indexOf(guess));
+              if (j === -1) {
+                lives -= 1;
+                comments();
+              } else {
+                comments();
+                }
+              userGuess.push(guess);
+              console.log(userGuess);
+              userGuessSection.innerHTML = userGuess; 
+          }
+        }
+      }
+    }
+  }
   userEvent();
+  play();
+
+}
+ 
+
+ 
 
   // Create guesses ul
   function result() {
     //li _ that will hold correct user guesses on the screen
-    wordHolder = document.getElementById('hold');
     correct = document.createElement('ul');
 
     for (var i = 0; i < word.length; i++) {
@@ -42,50 +78,26 @@
       correct.appendChild(guess);
     }
   }
-  
-  // Show lives
+
+
+
+   // Show lives
    comments = function () {
     showLives.innerHTML = "You have " + lives + " guesses remaining";
     if (lives < 1) {
       showLives.innerHTML = "Boo, none for you Glen Coco. Refresh the page to try again.";
+      acceptInput = false;
+
     }
     for (var i = 0; i < guesses.length; i++) {
       if (counter + space === guesses.length) {
-        showLives.innerHTML = "Ace! You got your fancy drink and a nod of approval. Refresh the page to play again.";
+        showLives.innerHTML = "Ace! You got your fancy drink and a nod of approval.";
+        acceptInput = false;
       }
     }
   }
 
 
-  //onkeyup function
-  function userEvent(){
-    document.onkeyup = function(event) {
-      var guess = String.fromCharCode(event.keyCode).toLowerCase();
-      if(accepted.includes(guess)){
-        if(userGuess.indexOf(guess) == -1){
-          for (var i = 0; i < word.length; i++) {
-            //replace word guess space with correct letter
-            if (word[i] === guess) {
-                guesses[i].innerHTML = guess;
-                counter += 1;
-                } 
-          } 
-            var j = (word.indexOf(guess));
-            if (j === -1) {
-              lives -= 1;
-              comments();
-            } else {
-              comments();
-              }
-            userGuess.push(guess);
-            console.log(userGuess);
-            userGuessSection.innerHTML = userGuess; 
-        }
-      }
-    } 
-  }
-  
-    
   // Play
   play = function () {
     categories = ["quinoa", "bespoke", "sushi", "sustainable", "totally", "rush", "frappuccino", "lulu lemon", "greek life", "leggings", "juice cleanse"];
@@ -102,10 +114,18 @@
     comments();
   }
 
-  play();
-  
-}
 
-function reset(){
-//triggered when comment won or loss
+
+ function resetButton(){
+  userGuess.length = 0;
+  acceptInput = true;
+  var wordUl = document.getElementById("my-word");
+
+  function removeUl() {
+      wordUl.parentNode.removeChild(wordUl);
+  }
+
+  removeUl();
+  play();
+
 }
